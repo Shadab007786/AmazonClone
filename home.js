07095e5f90e688a07ab7ea94b5507677399e93d6
Shadab <ahmadshadab353@gmail.com>
2022-12-13 12:13:07 +0530

    code

--- home.js
+++ home.js
@@ -7,3 +7,15 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+
+// coupon code
+
+function loadCoupon(){
+  document.getElementById('coupon').style.visibility = 'visible';
+  document.getElementById('main').style.opacity = '0.9';
+}
+
+function closeCoupon(){
+  document.getElementById('coupon').style.visibility = 'hidden';
+  document.getElementById('main').style.opacity = '1';
+}
